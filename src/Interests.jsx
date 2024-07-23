5a7ca7f1fe31cfa94eb67c464ec69cce815fd5f1
whois-plt <whois.plt@gmail.com>
2024-07-23 14:57:50 +0200

    information changes regarding About and Interests round 2

--- src/Interests.jsx
+++ src/Interests.jsx
@@ -6,11 +6,11 @@ const Interests = () => {
       <h2>Interests</h2>
       <p className="desc">
         I really love music. I work out with music, I work with music, I read with music, I sleep with music and I
-        couldn't imagine driving a car without music (think I didn't miss anything😄). I was also a drummer for quite a
+        couldn't imagine driving a car without music (hope I covered everything 😄). I was also a drummer for quite a
         few years, but that was a long time ago. Perhaps will do some more in the future.
         <br />
         <br />A few other things besides music: hiking, cooking, working out (mostly weight-lifting or calisthenics),
-        and smoking high-quality cigars.
+        chilling with friends, and smoking high-quality cigars.
       </p>
     </div>
   )
